72920ba5f8e746843d5ff048b33b664a23f772c1
Quadgence <quadgence@gmail.com>
2026-01-22 07:43:00 +0600

    gemini-2.5-flash

--- backend/agent/model.py
+++ backend/agent/model.py
@@ -1,4 +1,4 @@
 
 from langchain.chat_models import init_chat_model
 # LLM
-model = init_chat_model(model="gemini-3-flash-preview", temperature=0, model_provider='google_genai')
+model = init_chat_model(model="gemini-2.5-flash", temperature=0, model_provider='google_genai')
